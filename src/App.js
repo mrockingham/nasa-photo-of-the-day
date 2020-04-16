8d505632fe7a31b5bd9c18617ab6ec2c0b5dbb6e
Michael Rockingham <mrockingham2@gmail.com>
2020-04-16 12:40:14 -0400

    Starting to understand

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
+import PhotoList from 'C:/Users/User/desktop/lambdaflex/nasa-photo-of-the-day/src/PhotoList.js'
 import "./App.css";
-
+import logo from 'C:/Users/User/desktop/lambdaflex/nasa-photo-of-the-day/src/Icons/nasa logo.jpg'
 function App() {
   return (
     <div className="App">
+      <div className='Logo-Container'>
+        <img className='logo' src={logo} alt='Nasa logo' />
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+     </div> 
     </div>
   );
 }
---
+++ src/PhotoList.js
@@ -0,0 +1,11 @@
+// ice
+
+import React from 'react'
+
+const PhotoList =() => {
+   // return <div = 'Film' />;
+
+}
+
+
+export default PhotoList;
